d19076727424564c9a30c5173cd98e12c4f25b96
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2025-09-30 09:36:04 -0300

    venv do python para backend

---
+++ venv/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/classify', methods=['GET'])
+def receber_dados():
+    dados = request.get_json()
+    emailcontent = dados.get('emailcontent')
+    return jsonify({
+        "mensagem": f"{emailcontent}"
+    })
+
+
+if __name__ == '__main__':
+    app.run()
