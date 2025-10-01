ba9c6e356a4005b4c41f21ddca012e2f20a1037f
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2025-09-30 22:12:46 -0300

    organizando arquivos

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
-
 @app.route('/classify', methods=['GET'])
 def receber_dados():
     dados = request.get_json()
