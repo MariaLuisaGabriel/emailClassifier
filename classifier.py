f23b38fdbf908d15e1741637ed59317218e6df42
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2025-09-30 22:50:00 -0300

    testando o emailcontent

--- classifier.py
+++ classifier.py
@@ -4,7 +4,8 @@ def receber_dados(request):
     emailcontent = request.get("query", {}).get("email")
     return {
         "statusCode": 200,
-        "body": jsonify({
-        "mensagem": f"{emailcontent}"
-    })
-}
+        "headers": {
+            "Content-Type": "text/plain"
+        },
+        "body": emailcontent
+    }
