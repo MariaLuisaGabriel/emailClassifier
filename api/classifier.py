b51b63ccb684903e37a1471153afeb3161e03eab
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2025-10-01 13:41:26 -0300

    última tentativa

--- api/classifier.py
+++ api/classifier.py
@@ -1,4 +1,7 @@
-def handler(request):
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+def handler(request: BaseHTTPRequestHandler):
     emailcontent = request.get("query", {}).get("email","vazio")
     return {
         "statusCode": 200,
